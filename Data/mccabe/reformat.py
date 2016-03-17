837d11a88ace2056211d7852191acc07c3ba41bc
rahlk <i.m.ralk@gmail.com>
2016-03-16 21:47:42 -0400

    autocommit

--- Data/mccabe/reformat.py
+++ Data/mccabe/reformat.py
@@ -6,8 +6,28 @@ import numpy  as np
 from os import walk
 from pdb import set_trace
 
+
+def explore(dir):
+  datasets = []
+  for (dirpath, dirnames, filenames) in walk(dir):
+    datasets.append(dirpath)
+
+  training = []
+  testing = []
+  for k in datasets[1:]:
+    train = [[dirPath, fname] for dirPath, _, fname in walk(k)]
+    test = [train[0][0] + '/' + train[0][1].pop(-1)]
+    training.append(
+        [train[0][0] + '/' + p for p in train[0][1] if not p == '.DS_Store'])
+    testing.append(test)
+  return training, testing
+
+
 def main():
     projects = [Name for _, Name, __ in walk('./')][0]
+    numData = len(projects)  # Number of data
+    one, two = explore(dir)
+    data = [one[i] + two[i] for i in xrange(len(one))]
     set_trace()
 
 if __name__=='__main__':
