b10e8caf086c161338bb725d98166d4d9ae561b0
rahlk <i.m.ralk@gmail.com>
2016-03-16 21:40:11 -0400

    autocommit

---
+++ Data/mccabe/reformat.py
@@ -0,0 +1,14 @@
+"""
+Change the dependent variable column to 1's and 0's
+"""
+import pandas as pd
+import numpy  as np
+from os import walk
+from pdb import set_trace
+
+def main():
+    projects = [Name for _, Name, __ in walk('./')][0]
+    set_trace()
+
+if __name__=='__main__':
+  main()
