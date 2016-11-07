fdb5277bd432f9762b2bf2774415a9e21ce99ddf
Rahul Krishna <i.m.ralk@gmail.com>
2016-11-07 09:36:25 -0500

    Bugfix in Hedge's G

--- src/stats/effect_size.py
+++ src/stats/effect_size.py
@@ -3,6 +3,7 @@ from pdb import set_trace
 import numpy as np
 import pandas as pd
 
+
 def pooled_sd(dist1, dist2):
     s1 = np.std(dist1)
     s2 = np.std(dist2)
@@ -49,14 +50,12 @@ def hedges_g_2(dist, small=0.38):
         See: https://en.wikipedia.org/wiki/Effect_size#Hedges.27_g
     """
 
-    set_trace()
-
     n1, n2 = 4, 4
     correct_bias = (1 - 3 / (4 * (n1 + n2) - 9))
 
     def hg(m1, m2, s1, s2):
         s = np.sqrt(((n1 - 1) * s1 ** 2 + (n2 - 1) * s2 ** 2) / (n1 + n2 - 2))
-        g_score = correct_bias * (m1 - m2) / (s + 1e-32)
+        g_score = correct_bias * (m1 - m2) / (s + 1e-32) # Adding 1e-32 handles 0 std
         return str(round(-g_score, 2)) if abs(g_score) >= small else " "
 
     all = []
@@ -68,11 +67,28 @@ def hedges_g_2(dist, small=0.38):
         pf_hg = hg(stats.values[2], stats.values[8], stats.values[3], stats.values[9])
         g_hg = hg(stats.values[4], stats.values[10], stats.values[5], stats.values[11])
 
-        all.append([id, round(stats.iloc[0], 2), round(stats.iloc[1], 2), round(stats.iloc[2], 2),  round(stats.iloc[3], 2), round(stats.iloc[4], 2), round(stats.iloc[5], 2),  round(stats.iloc[6], 2), round(stats.iloc[7], 2), round(stats.iloc[8], 2), pd_hg, pf_hg, g_hg])
+        all.append([id,  # Name
+                    # TCA+
+                    round(stats.values[0], 2), round(stats.values[1], 2),  # Pd (mean), Pd(std)
+                    round(stats.values[2], 2), round(stats.values[3], 2),  # Pf (mean), Pf(std)
+                    round(stats.values[4], 2), round(stats.values[5], 2),  # G (mean), G(std)
+                    # Bellwether
+                    round(stats.values[6], 2), round(stats.values[7], 2),  # Pd (mean), Pd(std)
+                    round(stats.values[8], 2), round(stats.values[9], 2),  # Pf (mean), Pf(std)
+                    round(stats.values[11], 2), round(stats.values[11], 2),  # G (mean), G(std)
+                    # Hedges' G (Pd),  Hedges' G (Pf),  Hedges' G (G)
+                    pd_hg, pf_hg, g_hg])
 
-    return pd.DataFrame(all, columns=["Name","Pd (mean)", "Pd (std)",
+    return pd.DataFrame(all, columns=["Name",
+                                      # TCA+
+                                      "Pd (mean)", "Pd (std)",
                                       "Pf (mean)", "Pf (std)",
                                       "G (mean)", "G (std)",
+                                      # Bellwether
+                                      "Pd (mean)", "Pd (std)",
+                                      "Pf (mean)", "Pf (std)",
+                                      "G (mean)", "G (std)",
+                                      # Hedge's G
                                       "Pd (Hedges's G)",
                                       "Pf (Hedges's G)",
                                       "G (Hedges's G)"])
@@ -80,7 +96,7 @@ def hedges_g_2(dist, small=0.38):
 
 def _test_effect_size():
     dist1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    dist2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    dist2 = [1.5, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1]
 
     g, g_eff = hedges_g(dist1, dist2)
     print("Hedge's G: {}| Small Effect: {}".format(g, g_eff))
