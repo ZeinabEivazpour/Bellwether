e2ebe6872e8c15c55cd61a84e5c018d7dd4d1e70
rahlk <i.m.ralk@gmail.com>
2016-12-31 17:11:31 +0530

    local commit

--- src/defects/TNB/tnb.py
+++ src/defects/TNB/tnb.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division
 import os
 import sys
 
-root = os.path.join(os.getcwd().split('src')[0], 'src')
+root = os.path.join(os.getcwd().split('src')[0], 'src/defects')
 if root not in sys.path:
     sys.path.append(root)
 
@@ -19,9 +19,26 @@ import pandas
 from plot.effort_plot import effort_plot
 
 
-def tnb():
+def target_details(test_set):
+    set_trace()
+    # return (lo, hi), mass
+
+
+def get_test_mass(test_set):
+    return
+
+
+def get_distance(ranges):
+    return
+
+
+def weight_training(weights, training_instance):
+    return
+
+
+def tnb(source, target, n_rep=12):
     """
-    TCA: Transfer Component Analysis
+    TNB: Transfer Naive Bayes
     :param source:
     :param target:
     :param n_rep: number of repeats
@@ -35,45 +52,15 @@ def tnb():
         val = []
         for src_name, src_path in source.iteritems():
             if not src_name == tgt_name:
+                set_trace()
                 src = list2dataframe(src_path.data)
                 tgt = list2dataframe(tgt_path.data)
                 pd, pf, g, auc = [], [], [], []
-                dcv_src, dcv_tgt = get_dcv(src, tgt)
-
-                for _ in xrange(n_rep):
-                    recall, loc = None, None
-                    norm_src, norm_tgt = smart_norm(src, tgt, dcv_src, dcv_tgt)
-                    _train, __test = map_transform(norm_src, norm_tgt)
-                    # for k in np.arange(0.1,1,0.1):
-                    actual, predicted, distribution = predict_defects(train=_train, test=__test)
-                    loc = tgt["$loc"].values
-                    loc = loc * 100 / np.max(loc)
-                    recall, loc, au_roc = get_curve(loc, actual, predicted)
-                    effort_plot(recall, loc,
-                                save_dest=os.path.abspath(os.path.join(root, "plot", "plots", tgt_name)),
-                                save_name=src_name)
-                    p_d, p_f, p_r, rc, f_1, e_d, _g, _ = abcd(actual, predicted, distribution)
-
-                    pd.append(p_d)
-                    pf.append(p_f)
-                    g.append(_g)
-                    auc.append(int(au_roc))
-
-                    # set_trace()
-
-                stats.append([src_name, int(np.mean(pd)), int(np.std(pd)),
-                              int(np.mean(pf)), int(np.std(pf)),
-                              int(np.mean(auc)), int(np.std(auc))])  # ,
-                # int(np.mean(g)), int(np.std(g))])
-
-        stats = pandas.DataFrame(sorted(stats, key=lambda lst: lst[0]),  # Sort by G Score
-                                 columns=["Name", "Pd (Mean)", "Pd (Std)",
-                                          "Pf (Mean)", "Pf (Std)",
-                                          "AUC (Mean)", "AUC (Std)"])  # ,
-        # "G (Mean)", "G (Std)"])
-        result.update({tgt_name: stats})
-    # set_trace()
-    return result
+                min_max, test_mass = target_details(tgt)
+                test_mass = get_test_mass(tgt)
+                distance = get_distance(ranges=min_max)
+                weighted_source = weight_training(distance, )
+                set_trace()
 
 
 def tnb_jur():
