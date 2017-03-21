76afbf74d0bf794e9d6508822661ef567325fa72
rahlk <i.m.ralk@gmail.com>
2017-03-20 20:23:23 -0400

    TSE updates

--- src/SEER/submit.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#BSUB -W 6000
-#BSUB -n 4
-#BSUB -o ./out.%J
-#BSUB -e ./err.%J
-rm err/*
-rm out/*
-
-bsub -W 100 -n 12 -q long -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J /share/rkrish11/miniconda/bin/python2.7 par_exec.py
